8f557f9e3a19c132dce65cd6a0ccd58fb3258d7f
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2019-05-01 13:29:14 +0100

    remove links from work detail view text

--- archive-app/src/app/work-detail.component.ts
+++ archive-app/src/app/work-detail.component.ts
@@ -21,11 +21,17 @@ export class WorkDetailComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
   }
+  stripAnchors(html:string): string {
+  	if (!html)
+  		return html
+  	return html.replace(/<a [^>]*>/g, '').replace(/<\\a>/g, '')
+  }
   ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
   	if (changes['id'] && changes['id'].currentValue) {
   		console.log(`work detail view for ${changes['id'].currentValue}`, changes)
   		this.recordsService.getWork(changes['id'].currentValue)
       .then(work => { this.work = work; 
+        this.work.description = this.stripAnchors(this.work.description)
         this.recordsService.getPerformancesOfWork(work)
         .then(performances => this.performances = performances
           .sort((a,b) => a.compareTo(b, 'prov:startedAtTime')));
@@ -33,8 +39,11 @@ export class WorkDetailComponent implements OnInit, OnChanges {
         .then(members => this.parts = members
           .sort((a,b) => a.compareTo(b, 'coll:part_rank')));
         this.recordsService.getComposersOfWork(work)
-        .then(composers => this.composers = composers
-          .sort((a,b) => a.compareTo(b, 'rdfs:label')));
+        .then(composers => {
+        	this.composers = composers
+          .sort((a,b) => a.compareTo(b, 'rdfs:label'))
+          this.composers.forEach((c) => c.description = this.stripAnchors(c.description))
+        });
       });
   	}
   }
